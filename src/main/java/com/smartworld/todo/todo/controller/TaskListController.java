d4de234edd996046f1f86fd977c52c7baef043ce
h4rar <anton.h4rar@gmail.com>
2020-10-22 06:54:00 +0700

    Добавлефильтрация с помошью querydsl

--- src/main/java/com/smartworld/todo/todo/controller/TaskListController.java
+++ src/main/java/com/smartworld/todo/todo/controller/TaskListController.java
@@ -1,9 +1,12 @@
 package com.smartworld.todo.todo.controller;
 
+import com.querydsl.core.types.Predicate;
 import com.smartworld.todo.todo.forms.TaskListForm;
 import com.smartworld.todo.todo.model.TaskList;
+import com.smartworld.todo.todo.repository.TaskListRepository;
 import com.smartworld.todo.todo.service.*;
 import org.springframework.data.domain.*;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +17,9 @@ import javax.validation.Valid;
 public class TaskListController {
 
     private final TaskLisService taskLisService;
+
     private final Validator validator;
+
 
     public TaskListController(TaskLisService taskLisService, Validator validator) {
         this.taskLisService = taskLisService;
@@ -22,31 +27,43 @@ public class TaskListController {
     }
 
     @GetMapping("/")
-    public Page<TaskList> getAll(@PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable) {
-        return taskLisService.getTaskList(pageable);
+    public Page<TaskList> getAll(
+            @QuerydslPredicate(root = TaskList.class) Predicate predicate,
+            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable
+    ) {
+        return taskLisService.getTaskList(predicate, pageable);
     }
 
     @PostMapping("/")
-    public Page<TaskList> addNewListTask(@Valid @RequestBody TaskListForm taskListForm, BindingResult bindingResult,
-            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable) {
+    public Page<TaskList> addNewListTask(
+            @Valid @RequestBody TaskListForm taskListForm, BindingResult bindingResult,
+            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable,
+            @QuerydslPredicate(root = TaskList.class) Predicate predicate
+    ) {
         validator.validationName(bindingResult);
         taskLisService.addNewListTask(taskListForm);
-        return getAll(pageable);
+        return getAll(predicate, pageable);
     }
 
     @PutMapping("/{id}")
-    public Page<TaskList> editListTask(@PathVariable Long id,
+    public Page<TaskList> editListTask(
+            @PathVariable Long id,
             @Valid @RequestBody TaskListForm taskListForm, BindingResult bindingResult,
-            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable) {
+            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable,
+            @QuerydslPredicate(root = TaskList.class) Predicate predicate
+    ) {
         validator.validationName(bindingResult);
         taskLisService.editListTask(id, taskListForm);
-        return getAll(pageable);
+        return getAll(predicate, pageable);
     }
 
     @DeleteMapping("/{id}")
-    public Page<TaskList> deleteListTask(@PathVariable Long id,
-            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable) {
+    public Page<TaskList> deleteListTask(
+            @PathVariable Long id,
+            @PageableDefault(sort = {"dateOfCreation"}, direction = Sort.Direction.ASC) Pageable pageable,
+            @QuerydslPredicate(root = TaskList.class) Predicate predicate
+    ) {
         taskLisService.deleteListTask(id);
-        return getAll(pageable);
+        return getAll(predicate, pageable);
     }
 }
