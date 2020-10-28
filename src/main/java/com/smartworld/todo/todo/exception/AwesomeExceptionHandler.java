519fd999ddd7f3e7d483191e68a9d180553162d3
h4rar <anton.h4rar@gmail.com>
2020-10-28 13:46:57 +0700

    Добавлен ExceptionHandler

---
+++ src/main/java/com/smartworld/todo/todo/exception/AwesomeExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.smartworld.todo.todo.exception;
+
+import com.smartworld.todo.todo.exception.*;
+import lombok.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AwesomeExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<AwesomeException> handleNotFoundException() {
+        return new ResponseEntity<>(new AwesomeException("Not found"), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EmptyReadyException.class)
+    protected ResponseEntity<AwesomeException> handleEmptyReadyException() {
+        return new ResponseEntity<>(new AwesomeException("Ready can not be empty!"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IncorrectNameException.class)
+    protected ResponseEntity<AwesomeException> handleIncorrectNameException() {
+        return new ResponseEntity<>(new AwesomeException("Name empty"), HttpStatus.BAD_REQUEST);
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class AwesomeException {
+        private String message;
+    }
+}
